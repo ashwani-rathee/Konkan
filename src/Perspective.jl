55e9766ca79b035fa414368384de78d170b32202
ashwani rathee <ab669522@gmail.com>
2023-07-28 00:18:49 +0530

    Adds checkerboard utils

--- src/Perspective.jl
+++ src/Perspective.jl
@@ -1,7 +1,27 @@
 module Perspective
 
-include("projection.jl")
+# using
+using ImageCorners
+using ImageMorphology
+using ImageFiltering
+using ImageDraw
+using Statistics
 
+# Includes
+include("projection.jl")
+include("checkerboard.jl")
+
+# from projection.jl
 export image_point, project_multiple_points
+
+# from checkboard.jl
+export innercorners, allcorners, markcorners
+export segboundariescheck
+export checkboundaries
+export process_image
+export nonmaxsuppresion
+export kxkneighboardhood
+export drawdots!
+export draw_rect
 
 end
